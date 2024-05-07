27d805288973fc917e6d6925964e7e9e650fce25
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-07 12:35:54 +0900

    fix: 全てタブをクリックした時のstatus の値を修正

--- src/app/operations/index.ts
+++ src/app/operations/index.ts
@@ -3,14 +3,13 @@ import {
   StatusKeyEnum,
   Todo,
   type FetchTodosResponse,
-  type FormTodoData,
+  type FormTodoData
 } from "@/app/types";
 
 export const fetchTodos = async (
   statusFilter: StatusFilter
 ): Promise<FetchTodosResponse> => {
-  const queryParam =
-    statusFilter === StatusKeyEnum.Enum.all ? "" : `status=${statusFilter}`;
+  const queryParam = `status=${statusFilter}`;
   const response = await fetch(
     `/api/todos/${queryParam ? "?" + queryParam : ""}`
   );
@@ -25,7 +24,7 @@ export const addTodo = async (todo: FormTodoData): Promise<Todo> => {
   const response = await fetch("/api/todos", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ ...todo, statusKey: StatusKeyEnum.Enum.incomplete }),
+    body: JSON.stringify({ ...todo, statusKey: StatusKeyEnum.Enum.incomplete })
   });
   if (!response.ok) {
     throw new Error("ToDoの追加に失敗しました");
@@ -37,7 +36,7 @@ export const updateTodo = async (updatedTodo: Todo): Promise<Todo> => {
   const response = await fetch(`/api/todos?id=${updatedTodo.id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(updatedTodo),
+    body: JSON.stringify(updatedTodo)
   });
   if (!response.ok) {
     throw new Error("ToDoの更新に失敗しました");
@@ -47,7 +46,7 @@ export const updateTodo = async (updatedTodo: Todo): Promise<Todo> => {
 
 export const deleteTodo = async (id: string): Promise<void> => {
   const response = await fetch(`/api/todos?id=${id}`, {
-    method: "DELETE",
+    method: "DELETE"
   });
   if (!response.ok) {
     throw new Error("ToDoの削除に失敗しました");
